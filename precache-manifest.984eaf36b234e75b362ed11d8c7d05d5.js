976efe0ce373b8b1fe49f632495a2dae856cfbb7
joy21fs <1017peilin.yu@gmai.com>
2022-03-05 20:40:09 +0800

    Updates

---
+++ precache-manifest.984eaf36b234e75b362ed11d8c7d05d5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9f4e9757422773d7475",
+    "url": "/quotecollector/static/js/main.8f6031eb.chunk.js"
+  },
+  {
+    "revision": "aac7654af4a254c333ef",
+    "url": "/quotecollector/static/js/runtime~main.1a68f5c5.js"
+  },
+  {
+    "revision": "b466d70e343ac3a57b5c",
+    "url": "/quotecollector/static/js/2.ff535763.chunk.js"
+  },
+  {
+    "revision": "2e1638d0de4b933e97307c37220b86d0",
+    "url": "/quotecollector/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quotecollector/precache-manifest.e49cb709a0a03ca93ee1693b5351b351.js"
+  "/quotecollector/precache-manifest.984eaf36b234e75b362ed11d8c7d05d5.js"
 );
 
 workbox.clientsClaim();
