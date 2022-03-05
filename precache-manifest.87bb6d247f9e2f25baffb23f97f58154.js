620045e26fe87e4c8706f32e43ccc6d976ebdd02
joy21fs <1017peilin.yu@gmai.com>
2022-03-05 19:44:57 +0800

    Updates

---
+++ precache-manifest.87bb6d247f9e2f25baffb23f97f58154.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "13140907404e8baf2f8d",
+    "url": "/quotecollector/static/js/main.ae1c34c5.chunk.js"
+  },
+  {
+    "revision": "aac7654af4a254c333ef",
+    "url": "/quotecollector/static/js/runtime~main.1a68f5c5.js"
+  },
+  {
+    "revision": "cf22b71e1cf9e6e0ee96",
+    "url": "/quotecollector/static/js/2.b2091732.chunk.js"
+  },
+  {
+    "revision": "7118fcd9fb55906ec2e2405e074b852d",
+    "url": "/quotecollector/index.html"
+  }
+];
